20b5f328a6953c532e7e9837123811c8953cfffb
Vince <vlaylo@luc.edu>
2019-11-25 15:14:44 -0600

    f

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
